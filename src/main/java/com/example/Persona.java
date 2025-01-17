9038e0021b97efbc29e6f9ecc57f67c291737b92
zoryon <gioelespata@gmail.com>
2025-01-17 16:13:08 +0100

    first commit

---
+++ src/main/java/com/example/Persona.java
@@ -0,0 +1,49 @@
+package com.example;
+
+public class Persona {
+    // attributi
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+
+    // costruttori
+    public Persona(String nome, String cognome, String codiceFiscale) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+    }
+    
+    public Persona() {
+        this.nome = "";
+        this.cognome = "";
+        this.codiceFiscale = "";
+    }
+
+    // getter & setter
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    // metodi
+
+}
